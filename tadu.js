02af2e330c3a0e350e15f80f268ae657cb5a0497
hyzaw <62041055+hyzaw@users.noreply.github.com>
2022-10-03 06:55:21 +0800

    Update tadu.js

--- tadu.js
+++ tadu.js
@@ -8,16 +8,16 @@
 var body = $response.body;
 var url = $request.url;
 if (url.indexOf('https://reader.tadu.com/book/chapter/getChapterTdz') !== -1) {
-    obj = JSON.parse(body)
-    obj.data.chapterInfo.isCleanAd = 1
-    obj.data.chapterInfo.isMember = 1
-    obj.data.chapterInfo.freeType = 1
-    obj.data.bookInfo.canRead = 1
-    $done(JSON.stringify(body));
+    obj = JSON.parse(body);
+    obj.data.chapterInfo.isCleanAd = "1";
+    obj.data.chapterInfo.isMember = 1;
+    obj.data.chapterInfo.freeType = 1;
+    obj.data.bookInfo.canRead = 1;
+    $done(JSON.stringify(body));;
 }else if(url.indexOf('https://reader.tadu.com/user/api/space/userInfo') !== -1){
-    obj = JSON.parse(body)
-    obj.data.isMember = 1;
-    $done(JSON.stringify(body));
+    obj = JSON.parse(body);
+    obj.data.isMember = 1;;
+    $done(JSON.stringify(body));;
 }
 
 $done(body);
