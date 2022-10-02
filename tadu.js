a67461fb3d718a333dec85499271d2f0ee6e6722
hyzaw <62041055+hyzaw@users.noreply.github.com>
2022-10-03 07:11:02 +0800

    Update tadu.js

--- tadu.js
+++ tadu.js
@@ -4,7 +4,6 @@
  * @supported FUCK-SUEP
  */
 
-
 var body = $response.body;
 var url = $request.url;
 if (url.indexOf("getChapterTdz") !== -1) {
@@ -13,8 +12,8 @@ if (url.indexOf("getChapterTdz") !== -1) {
     obj.data.chapterInfo["isMember"] = 1;
     obj.data.chapterInfo["freeType"] = 1;
     obj.data.bookInfo["canRead"] = 1;
-    $done(JSON.stringify(body));;
-}else if(url.indexOf("userInfo") !== -1){
+    $done(JSON.stringify(body));
+} else if (url.indexOf("userInfo") !== -1) {
     obj = JSON.parse(body);
     obj.data["isMember"] = 1;
     $done(JSON.stringify(body));
